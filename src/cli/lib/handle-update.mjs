f636515271bd71bb7adbe3903972f748d3c04283
Zane Rockenbaugh <zane@liquid-labs.com>
2024-02-29 10:58:03 -0600

    sketched in update action

---
+++ src/cli/lib/handle-update.mjs
@@ -0,0 +1,22 @@
+import commandLineArgs from 'command-line-args'
+
+import { cliSpec } from '../constants'
+import { update } from '../../lib/actions/update'
+
+const handleUpdate = async ({ argv, globalOptions, sitesInfo }) => {
+  const updateOptionsSpec = cliSpec.commands.find(({ name }) => name === 'update').arguments
+  const updateOptions = commandLineArgs(updateOptionsSpec, { argv })
+  const apexDomain = updateOptions['apex-domain']
+  const noCacheInvalidation = updateOptions['no-cache-invalidation']
+  const onlyContent = updateOptions['only-content']
+
+  const siteInfo = sitesInfo[apexDomain]
+  if (siteInfo === undefined) {
+    process.stderr.write(`No such site '${apexDomain}' found.`)
+    process.exit(1) // eslint-disable-line no-process-exit
+  }
+
+  await update({ noCacheInvalidation, onlyContent, sitesInfo, ...globalOptions })
+}
+
+export { handleUpdate }
