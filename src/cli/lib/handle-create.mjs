0b2e2f2cd0d2c5bc481d8db52a49f7b7616ab03d
Zane Rockenbaugh <zane@liquid-labs.com>
2024-02-29 10:57:45 -0600

    regularized use of siteInfo

--- src/cli/lib/handle-create.mjs
+++ src/cli/lib/handle-create.mjs
@@ -8,22 +8,20 @@ import { cliSpec, SOURCE_TYPES } from '../constants'
 import { create } from '../../lib/actions/create'
 
 const handleCreate = async ({ argv, globalOptions, sitesInfo }) => {
-  // build up the options
-  const options = structuredClone(globalOptions)
-
   const createOptionsSpec = cliSpec.commands.find(({ name }) => name === 'create').arguments
   const createOptions = commandLineArgs(createOptionsSpec, { argv })
-  options.apexDomain = createOptions['apex-domain']
-  options.bucketName = createOptions['bucket-name']
-  options.noDeleteOnFailure = createOptions['no-delete-on-failure']
-  options.sourcePath = createOptions['source-path']
-  options.sourceType = createOptions['source-type']
+  // action behavior options
+  const noDeleteOnFailure = createOptions['no-delete-on-failure']
+  // siteInfo options
+  const apexDomain = createOptions['apex-domain']
+  const bucketName = createOptions['bucket-name']
+  const sourcePath = createOptions['source-path']
+  let sourceType = createOptions['source-type']
 
-  const siteInfo = sitesInfo[options.apexDomain] || { apexDomain : options.apexDomain }
-  sitesInfo[options.apexDomain] = siteInfo
-  options.siteInfo = siteInfo
+  const siteInfo = sitesInfo[apexDomain] || { apexDomain, bucketName, sourcePath, sourceType }
+  siteInfo.region = createOptions.region || siteInfo.region || 'us-east-1'
 
-  options.region = createOptions.region || siteInfo.region || 'us-east-1'
+  sitesInfo[apexDomain] = siteInfo
 
   // verify the parameters/options
   for (const option of ['apex-domain', 'source-path']) {
@@ -35,20 +33,20 @@ const handleCreate = async ({ argv, globalOptions, sitesInfo }) => {
   }
   // TODO: verify apex domain matches apex domain RE
 
-  if (options.sourceType === undefined) {
-    const docusaurusConfigPath = fsPath.resolve(options.sourcePath, '..', 'docusaurus.config.js')
-    options.sourceType = fileExists(docusaurusConfigPath) ? 'docusaurus' : 'vanilla'
-  } else if (!SOURCE_TYPES.includes(options.sourceType)) {
-    process.stderr(`Invalid site source type '${options.sourceType}'; must be one of ${SOURCE_TYPES.join(', ')}.\n`)
+  if (sourceType === undefined) {
+    const docusaurusConfigPath = fsPath.resolve(sourcePath, '..', 'docusaurus.config.js')
+    sourceType = fileExists(docusaurusConfigPath) ? 'docusaurus' : 'vanilla'
+  } else if (!SOURCE_TYPES.includes(sourceType)) {
+    process.stderr(`Invalid site source type '${sourceType}'; must be one of ${SOURCE_TYPES.join(', ')}.\n`)
     process.exit(2) // eslint-disable-line no-process-exit
   }
 
-  if (options.bucketName !== undefined && !awsS3TABucketNameRE.test(options.bucketName)) {
+  if (bucketName !== undefined && !awsS3TABucketNameRE.test(bucketName)) {
     process.stderr(`Invalid bucket name. Must be valid AWS S3 Transfer Accelerated bucket name matching: ${awsS3TABucketNameREString}`)
     process.exit(2) // eslint-disable-line no-process-exit
   }
 
-  await create(options)
+  await create({ noDeleteOnFailure, siteInfo, ...globalOptions })
 }
 
 export { handleCreate }
