d26b7dd5dc62168b03f65cb2df2a644a040d5831
Zane Rockenbaugh <zane@liquid-labs.com>
2024-03-12 18:03:27 -0500

    update 'command' to 'subcommand' in the configuration handler

--- src/cli/lib/handle-configuration.mjs
+++ src/cli/lib/handle-configuration.mjs
@@ -7,16 +7,16 @@ const handleConfiguration = async ({ argv, cliSpec, globalOptions }) => {
   const configurationCLISpec = cliSpec.commands.find(({ name }) => name === 'configuration')
   const configurationOptionsSpec = configurationCLISpec.arguments
   const configurationOptions = commandLineArgs(configurationOptionsSpec, { argv, stopAtFirstUnknown : true })
-  const { command } = configurationOptions
+  const { subcommand } = configurationOptions
   argv = configurationOptions._unknown || []
 
-  switch (command) {
+  switch (subcommand) {
     case 'initialize':
       await handleConfigurationInitialize({ argv, configurationCLISpec, globalOptions }); break
     case 'show':
       await handleConfigurationShow({ argv, configurationCLISpec, globalOptions }); break
     default:
-      throw new Error('Unknown configuration command: ' + command)
+      throw new Error('Unknown configuration command: ' + subcommand)
   }
 }
 
