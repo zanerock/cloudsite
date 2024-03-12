e33d7949272465ddf597db9fd6c8b7979ed94da2
Zane Rockenbaugh <zane@liquid-labs.com>
2024-03-11 19:42:48 -0500

    encapsulated the emailer function setup

--- src/lib/plugins/contact-handler/index.mjs
+++ src/lib/plugins/contact-handler/index.mjs
@@ -1,7 +1,7 @@
 import { emailRE } from 'regex-repo'
 
-import { CONTACT_EMAILER_ZIP_NAME } from './lib/constants'
 import { setupContactFormTable } from './lib/setup-contact-form-table'
+import { setupContactEmailer } from './lib/setup-contact-emailer'
 import { setupContactHandler } from './lib/setup-contact-handler'
 import { setupRequestSigner } from './lib/setup-request-signer'
 import { stageLambdaFunctionZipFiles } from './lib/stage-lambda-function-zip-files'
@@ -20,14 +20,8 @@ const config = {
 }
 
 const stackConfig = async ({ siteTemplate, settings }) => {
-  const { finalTemplate, siteInfo } = siteTemplate
-  const contactHandlerFromEmail = settings.emailFrom
-  const contactHandlerTargetEmail = settings.emailTo
-  const enableEmail = !!contactHandlerFromEmail
-
-  if (enableEmail !== true && contactHandlerTargetEmail) {
-    throw new Error("Found site setting for 'emailTo', but no 'emailFrom'; 'emailFrom' must be set to activate email functionality.")
-  }
+  const { siteInfo } = siteTemplate
+  const enableEmail = !!settings.emailFrom
 
   const lambdaFunctionsBucketName = await stageLambdaFunctionZipFiles({ enableEmail, siteInfo })
 
@@ -35,109 +29,9 @@ const stackConfig = async ({ siteTemplate, settings }) => {
   setupRequestSigner({ lambdaFunctionsBucketName, siteInfo })
   setupContactFormTable({ siteInfo })
   updateCloudFrontDistribution({ settings, siteInfo })
-
-  // add the email trigger on new DynamoDB entries
-  if (contactHandlerFromEmail !== undefined) {
-    // setup stream on table
-    finalTemplate.Resources.ContactHandlerDynamoDB.Properties.StreamSpecification = {
-      StreamViewType : 'NEW_IMAGE'
-    }
-
-    const emailerFunctionName = lambdaFunctionsBucketName + '-contact-emailer'
-    const emailerFunctionLogGroupName = emailerFunctionName
-
-    finalTemplate.Resources.ContactEmailerLogGroup = {
-      Type       : 'AWS::Logs::LogGroup',
-      Properties : {
-        LogGroupClass   : 'STANDARD', // TODO: support option for INFREQUENT_ACCESS
-        LogGroupName    : emailerFunctionLogGroupName,
-        RetentionInDays : 180 // TODO: support options
-      }
-    }
-
-    finalTemplate.Resources.ContactEmailerRole = {
-      Type       : 'AWS::IAM::Role',
-      Properties : {
-        AssumeRolePolicyDocument : {
-          Version   : '2012-10-17',
-          Statement : [
-            {
-              Effect    : 'Allow',
-              Principal : {
-                Service : ['lambda.amazonaws.com']
-              },
-              Action : ['sts:AssumeRole']
-            }
-          ]
-        },
-        Path     : '/',
-        Policies : [
-          {
-            PolicyName     : lambdaFunctionsBucketName + '-contact-handler',
-            PolicyDocument : {
-              Version   : '2012-10-17',
-              Statement : [
-                {
-                  Effect   : 'Allow',
-                  Action   : ['ses:SendEmail', 'ses:SendEmailRaw', 'ses:GetSendQuota', 'ses:GetSendStatistics'],
-                  Resource : '*'
-                }
-              ]
-            }
-          }
-        ],
-        ManagedPolicyArns : [
-          // AWSLambdaBasicExecutionRole: allows logging to CloudWatch
-          'arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole',
-          // Allows reading from DynamoDB streams
-          'arn:aws:iam::aws:policy/service-role/AWSLambdaDynamoDBExecutionRole'
-        ]
-      }
-    }
-
-    finalTemplate.Resources.ContactEmailerFunction = {
-      Type       : 'AWS::Lambda::Function',
-      DependsOn  : ['ContactEmailerRole', 'ContactEmailerLogGroup'],
-      Properties : {
-        FunctionName : emailerFunctionName,
-        Handler      : 'index.handler',
-        Role         : { 'Fn::GetAtt' : ['ContactEmailerRole', 'Arn'] },
-        Runtime      : 'nodejs20.x',
-        MemorySize   : 128,
-        Timeout      : 5,
-        Code         : {
-          S3Bucket : lambdaFunctionsBucketName,
-          S3Key    : CONTACT_EMAILER_ZIP_NAME
-        },
-        Environment : {
-          Variables : {
-            EMAIL_HANDLER_SOURCE_EMAIL : contactHandlerFromEmail
-          }
-        },
-        LoggingConfig : {
-          ApplicationLogLevel : 'INFO', // support options
-          LogFormat           : 'JSON', // support options
-          LogGroup            : emailerFunctionLogGroupName,
-          SystemLogLevel      : 'INFO' // support options
-        }
-      }
-    }
-
-    finalTemplate.Resources.ContactEmailerEventsSource = {
-      Type       : 'AWS::Lambda::EventSourceMapping',
-      DependsOn  : ['ContactEmailerFunction'],
-      Properties : {
-        FunctionName     : { 'Fn::GetAtt' : ['ContactEmailerFunction', 'Arn'] },
-        EventSourceArn   : { 'Fn::GetAtt' : ['ContactHandlerDynamoDB', 'StreamArn'] },
-        StartingPosition : 'LATEST'
-      }
-    }
-
-    if (contactHandlerTargetEmail !== undefined) {
-      finalTemplate.Resources.ContactEmailerFunction.Properties.Environment.Variables.EMAIL_HANDLER_TARGET_EMAIL =
-        contactHandlerTargetEmail
-    }
-  } // if (contactHandlerFromEmail !== undefined) {
+  if (enableEmail === true) {
+    setupContactEmailer({ lambdaFunctionsBucketName, settings, siteInfo })
+  }
 }
 
 const contactHandler = { config, stackConfig }
