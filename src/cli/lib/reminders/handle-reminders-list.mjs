299ffafd1d37e088206b0d808ec11ec25183732b
Zane Rockenbaugh <zane@liquid-labs.com>
2024-04-09 17:28:19 -0500

    lint fixes

--- src/cli/lib/reminders/handle-reminders-list.mjs
+++ src/cli/lib/reminders/handle-reminders-list.mjs
@@ -1,9 +1,9 @@
 import { todoTranslator } from './todo-translator'
 
 const handleRemindersList = ({ db }) => {
-  const todoList = db.reminders.map(({ command, ...todoEntry }) => ({ todo: todoTranslator(todoEntry), command }))
+  const todoList = db.reminders.map(({ command, ...todoEntry }) => ({ todo : todoTranslator(todoEntry), command }))
 
-  return { data: todoList, success : true }
+  return { data : todoList, success : true }
 }
 
 export { handleRemindersList }
--- src/cli/lib/reminders/todo-translator.mjs
+++ src/cli/lib/reminders/todo-translator.mjs
@@ -1,13 +1,13 @@
 import { ACTION_CLEANUP, ACTION_SETUP_BILLING } from '../../constants'
 
-const todoTranslator = ({ command, references, todo }) => {
+const todoTranslator = ({ references, todo }) => {
   switch (todo) {
-  case ACTION_CLEANUP:
-    return `Cleanup partially deleted site '${references}'.`
-  case ACTION_SETUP_BILLING:
-    return `Setup billing tags for '${references}'.`
-  default:
-    throw new Error(`Unrecognized reminder type (todo): '${todo}'`)
+    case ACTION_CLEANUP:
+      return `Cleanup partially deleted site '${references}'.`
+    case ACTION_SETUP_BILLING:
+      return `Setup billing tags for '${references}'.`
+    default:
+      throw new Error(`Unrecognized reminder type (todo): '${todo}'`)
   }
 }
 
