a5743e1cdb03ebde28964d98abbc45bcc4da46a8
Zane Rockenbaugh <zane@liquid-labs.com>
2024-04-16 15:03:13 -0500

    first draft complete SSO account setup

--- src/lib/actions/setup-sso.mjs
+++ src/lib/actions/setup-sso.mjs
@@ -2,9 +2,16 @@ import { AccountClient, ListRegionsCommand } from '@aws-sdk/client-account'
 import { CreatePolicyCommand, IAMClient, ListPoliciesCommand } from '@aws-sdk/client-iam'
 import { CreateGroupCommand, IdentitystoreClient, ListGroupsCommand } from '@aws-sdk/client-identitystore'
 import {
+  AttachCustomerManagedPolicyReferenceToPermissionSetCommand,
+  CreateAccountAssignmentCommand,
   CreateInstanceCommand,
+  CreatePermissionSetCommand,
   DescribeInstanceCommand,
+  DescribePermissionSetCommand,
+  ListAccountAssignmentsCommand,
+  ListCustomerManagedPolicyReferencesInPermissionSetCommand,
   ListInstancesCommand,
+  ListPermissionSetsCommand,
   SSOAdminClient
 } from '@aws-sdk/client-sso-admin'
 
@@ -15,10 +22,60 @@ import { progressLogger } from '../shared/progress-logger'
 const setupSSO = async ({ db, groupName, instanceName, instanceRegion, policyName, ssoProfile, userName }) => {
   const credentials = getCredentials()
 
+  const { accountID } = db.account
+
   const policyARN = await setupPolicy({ credentials, db, policyName })
-  const { identityStoreID, identityStoreRegion } =
+  const { identityStoreID, identityStoreRegion, instanceARN, ssoAdminClient } =
     await setupIdentityStore({ credentials, instanceName, instanceRegion })
   const groupID = await setupSSOGroup({ credentials, identityStoreID, identityStoreRegion, groupName })
+  const permissionSetARN = await setupPermissionSet({ credentials, instanceARN, policyARN, policyName, ssoAdminClient })
+  await setupAccountAssignment({ accountID, groupID, instanceARN, permissionSetARN, ssoAdminClient })
+}
+
+const setupAccountAssignment = async ({ accountID, groupID, instanceARN, permissionSetARN, ssoAdminClient }) => {
+  progressLogger.write('Checking for account assignment...')
+
+  let found = false
+  let nextToken
+  do {
+    const listAccountAssignmentsCommand = new ListAccountAssignmentsCommand({
+      InstanceArn : instanceARN,
+      AccountId   : accountID,
+      PermissionSetArn: permissionSetARN,
+      NextToken   : nextToken
+    })
+    const listAccountAssignmentsResults = await ssoAdminClient.send(listAccountAssignmentsCommand)
+    for (const { PrincipalType: principalType, PrincipalId: principalID }
+      of listAccountAssignmentsResults.AccountAssignments) {
+      if (principalType === 'GROUP' && principalID === groupID) {
+        found = true
+      }
+    }
+  } while (found === false && nextToken !== undefined)
+
+  if (found === true) {
+    progressLogger.write(' FOUND.\n')
+  }
+  else {
+    progressLogger.write(' CREATING...')
+    const createAccountAssignmentCommand = new CreateAccountAssignmentCommand({
+      InstanceArn: instanceARN,
+      TargetId: accountID,
+      TargetType: 'AWS_ACCOUNT',
+      PermissionSetArn: permissionSetARN,
+      PrincipalType: 'GROUP',
+      PrincipalId: groupID
+    })
+
+    try {
+      await ssoAdminClient.send(createAccountAssignmentCommand)
+      progressLogger.write(' DONE.\n')
+    }
+    catch (e) {
+      progressLogger.write(' ERROR.\n')
+      throw e
+    }
+  }
 }
 
 const setupPolicy = async ({ credentials, db, policyName }) => {
@@ -75,8 +132,9 @@ const setupPolicy = async ({ credentials, db, policyName }) => {
 const setupIdentityStore = async ({ credentials, instanceName, instanceRegion }) => {
   progressLogger.write('Checking for SSO identity store...')
 
-  let nextToken, identityStoreID, identityStoreRegion
+  let nextToken, identityStoreID, identityStoreRegion, instanceARN
   const accountClient = new AccountClient({ credentials })
+  let ssoAdminClient
 
   do {
     const listRegionsCommand = new ListRegionsCommand({
@@ -107,12 +165,13 @@ const setupIdentityStore = async ({ credentials, instanceName, instanceRegion })
     })
 
     for (const { RegionName: region } of regions) {
-      const ssoAdminClient = new SSOAdminClient({ credentials, region })
+      ssoAdminClient = new SSOAdminClient({ credentials, region })
       const listInstancesCommand = new ListInstancesCommand({})
       const listInstancesResult = await ssoAdminClient.send(listInstancesCommand)
       if (listInstancesResult.Instances.length > 0) {
         const instance = listInstancesResult.Instances[0]
         identityStoreID = instance.IdentityStoreId
+        instanceARN = instance.InstanceArn
         identityStoreRegion = region
         break
       }
@@ -125,11 +184,10 @@ const setupIdentityStore = async ({ credentials, instanceName, instanceRegion })
   } else {
     progressLogger.write(' CREATING...')
 
-    const ssoAdminClient = new SSOAdminClient({ credentials, region : instanceRegion })
     const createInstanceCommand = new CreateInstanceCommand({ Name : instanceName })
     try {
-      const createInstanceResults = await ssoAdminClient.send(createInstanceCommand)
-      const { InstanceArn: instanceARN } = createInstanceResults
+      const createInstanceResults = await ssoAdminClient.send(createInstanceCommand);
+      ({ InstanceArn: instanceARN } = createInstanceResults)
 
       const describeInstanceCommand = new DescribeInstanceCommand({ InstanceArn : instanceARN })
       const describeInstanceResults = await ssoAdminClient.send(describeInstanceCommand)
@@ -144,7 +202,118 @@ const setupIdentityStore = async ({ credentials, instanceName, instanceRegion })
     }
   }
 
-  return { identityStoreID, identityStoreRegion }
+  return { identityStoreID, identityStoreRegion, instanceARN, ssoAdminClient }
+}
+
+const setupPermissionSet = async ({ credentials, instanceARN, policyARN, policyName, ssoAdminClient }) => {
+  progressLogger.write(`Looking for permission set '${policyName}'...`)
+  let nextToken, permissionSetARN
+  do {
+    const listPermissionSetsCommand = new ListPermissionSetsCommand({ InstanceArn : instanceARN })
+    const listPermissionSetsResult = await ssoAdminClient.send(listPermissionSetsCommand)
+    for (const testARN of listPermissionSetsResult.PermissionSets) {
+      const describePermissionSetCommand = new DescribePermissionSetCommand({
+        InstanceArn      : instanceARN,
+        PermissionSetArn : testARN
+      })
+      const { Name: name } = (await ssoAdminClient.send(describePermissionSetCommand)).PermissionSet
+
+      if (name === policyName) {
+        permissionSetARN = testARN
+        break
+      }
+    }
+    nextToken = listPermissionSetsResult.NextToken
+  } while (permissionSetARN === undefined && nextToken !== undefined)
+
+  if (permissionSetARN !== undefined) {
+    progressLogger.write(' FOUND.\n')
+    await setupPermissionSetPolicy({
+      instanceARN,
+      permissionSetARN,
+      policyARN,
+      policyName,
+      searchExisting : true,
+      ssoAdminClient
+    })
+  } else {
+    progressLogger.write(' CREATING...')
+
+    const createPermissionSetCommand = new CreatePermissionSetCommand({
+      Name        : policyName,
+      InstanceArn : instanceARN
+    })
+    try {
+      permissionSetARN = (await ssoAdminClient.send(createPermissionSetCommand)).PermissionSet.PermissionSetArn
+      progressLogger.write(' CREATED.\n')
+      await setupPermissionSetPolicy({
+        instanceARN,
+        permissionSetARN,
+        policyARN,
+        policyName,
+        searchExisting : false,
+        ssoAdminClient
+      })
+    } catch (e) {
+      progressLogger.write(' ERROR.\n')
+      throw e
+    }
+  }
+
+  return permissionSetARN
+}
+
+const setupPermissionSetPolicy = async ({
+  instanceARN,
+  permissionSetARN,
+  policyARN,
+  policyName,
+  searchExisting,
+  ssoAdminClient
+}) => {
+  progressLogger.write(`Checking permission set '${policyName}' associated policies...`)
+  let policyFound = false
+  if (searchExisting === true) {
+    let nextToken
+    do {
+      const listCustomerManagedPolicyReferencesInPermissionSetCommand =
+        new ListCustomerManagedPolicyReferencesInPermissionSetCommand({
+          InstanceArn      : instanceARN,
+          PermissionSetArn : permissionSetARN,
+          NextToken        : nextToken
+        })
+
+      const listCustomerManagedPolicyReferencesInPermissionSetResults =
+        await ssoAdminClient.send(listCustomerManagedPolicyReferencesInPermissionSetCommand)
+
+      for (const { Name : name } of
+        listCustomerManagedPolicyReferencesInPermissionSetResults.CustomerManagedPolicyReferences) {
+        if (name === policyName) {
+          policyFound = true
+        }
+      }
+      nextToken = listCustomerManagedPolicyReferencesInPermissionSetResults.NextToken
+    } while (policyFound === false && nextToken !== undefined)
+  }
+
+  if (policyFound === true) {
+    progressLogger.write(' FOUND.\n')
+  } else {
+    progressLogger.write(' ASSOCIATING...')
+    const attachCustomerManagedPolicyReferenceToPermissionSetCommand =
+      new AttachCustomerManagedPolicyReferenceToPermissionSetCommand({
+        InstanceArn                    : instanceARN,
+        PermissionSetArn               : permissionSetARN,
+        CustomerManagedPolicyReference : { Name : policyName }
+      })
+    try {
+      await ssoAdminClient.send(attachCustomerManagedPolicyReferenceToPermissionSetCommand)
+      progressLogger.write(' DONE.\n')
+    } catch (e) {
+      progressLogger.write(' ERROR.\n')
+      throw e
+    }
+  }
 }
 
 const setupSSOGroup = async ({ credentials, identityStoreID, identityStoreRegion, groupName }) => {
