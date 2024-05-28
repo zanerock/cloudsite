927db54f42f2d939cc0ba6606e3a78382017b97e
Zane Rockenbaugh <zane@liquid-labs.com>
2024-05-28 12:54:45 -0500

    support tags without setting function (for stack level tags)

--- src/lib/shared/get-resource-tags.mjs
+++ src/lib/shared/get-resource-tags.mjs
@@ -1,7 +1,12 @@
-const getResourceTags = ({ funcDesc, siteInfo }) => [
-  { Key : 'application', value : 'Cloudsite' },
-  { Key : 'site', Value : siteInfo.apexDomain },
-  { Key : 'function', Value : funcDesc }
-]
+const getResourceTags = ({ funcDesc, siteInfo }) => {
+  const tags = [
+    { Key : 'application', Value : 'Cloudsite' },
+    { Key : 'site', Value : siteInfo.apexDomain }
+  ]
+  if (funcDesc !== undefined) {
+    tags.push({ Key : 'function', Value : funcDesc })
+  }
+  return tags
+}
 
 export { getResourceTags }
