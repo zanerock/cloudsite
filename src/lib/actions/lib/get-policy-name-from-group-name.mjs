d005cf1b69c9a6c42e21414a4d97ae5e22feb581
Zane Rockenbaugh <zane@liquid-labs.com>
2024-06-08 18:51:31 -0500

    added test and simplified getPolicyNameFromGroupName

--- src/lib/actions/lib/get-policy-name-from-group-name.mjs
+++ src/lib/actions/lib/get-policy-name-from-group-name.mjs
@@ -1,7 +1,6 @@
 import camelCase from 'lodash/camelCase'
 import upperFirst from 'lodash/upperFirst'
 
-const getPolicyNameFromGroupName = (groupName) =>
-  upperFirst(camelCase(groupName)).replaceAll(/[^a-zA-Z0-9_+=,.@-]/g, '').slice(0, 128)
+const getPolicyNameFromGroupName = (groupName) => upperFirst(camelCase(groupName)).slice(0, 128)
 
 export { getPolicyNameFromGroupName }
--- src/lib/actions/lib/test/get-policy-name-from-group-name.test.mjs
+++ src/lib/actions/lib/test/get-policy-name-from-group-name.test.mjs
@@ -3,6 +3,7 @@ import { getPolicyNameFromGroupName } from '../get-policy-name-from-group-name'
 describe('getPolicyNameFromGroupName', () => {
   test.each([
     ['Content manager group', 'ContentManagerGroup'],
+    ['CS:Foo admin group', 'CsFooAdminGroup'],
     ['AReallyLongGroupNameAReallyLongGroupNameAReallyLongGroupNameAReallyLongGroupNameAReallyLongGroupNameAReallyLongGroupNameAReallyLongGroupName', 'AReallyLongGroupNameAReallyLongGroupNameAReallyLongGroupNameAReallyLongGroupNameAReallyLongGroupNameAReallyLongGroupNameAReallyL']
   ])('%s -> %s', (input, expected) => expect(getPolicyNameFromGroupName(input)).toBe(expected))
 })
